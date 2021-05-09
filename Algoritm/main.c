2ba5242cec92735552eda5adaf60fd19bf01acdc
OctSkyOut <kms3335k@naver.com>
2021-05-09 14:25:53 +0900

    Initial Commit

---
+++ Algoritm/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Algoritm
+//
+//  Created by Oct_Sky_Out on 2021/05/09.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
